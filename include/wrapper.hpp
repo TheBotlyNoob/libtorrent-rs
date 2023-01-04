e127b90cb1217c642705cbeb165b5ce88de10551
Jay D Jackson <jj@jj.is-a.dev>
2023-01-04 18:00:05 +0000

    building libtorrent

---
+++ include/wrapper.hpp
@@ -0,0 +1,6 @@
+#include <libtorrent/session.hpp>
+#include <libtorrent/session_params.hpp>
+#include <libtorrent/add_torrent_params.hpp>
+#include <libtorrent/torrent_handle.hpp>
+#include <libtorrent/alert_types.hpp>
+#include <libtorrent/magnet_uri.hpp>
