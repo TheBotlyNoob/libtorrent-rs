e086c483e2b8977bf3050787e19e7a79f58ebbc4
Jay Jackson <jj@jj.is-a.dev>
2023-02-20 22:51:50 -0500

    WIP autocxx

--- libtorrent-sys/build.rs
+++ libtorrent-sys/build.rs
@@ -1,8 +1,11 @@
-use std::{env, path::PathBuf};
+use std::env;
 
 use cmake::Config;
 
-fn main() {
+fn main() -> miette::Result<()> {
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed=src/lib.rs");
+
     let mut conf = Config::new("vendor/libtorrent");
 
     #[cfg(not(debug_assertions))]
@@ -22,28 +25,8 @@ fn main() {
     );
     println!("cargo:rustc-link-lib=static=torrent-rasterbar");
 
-    let incl = dst.join("include");
-
-    // The bindgen::Builder is the main entry point
-    // to bindgen, and lets you build up options for
-    // the resulting bindings.
-    let bindings = bindgen::Builder::default()
-        // The input header we would like to generate
-        // bindings for.
-        .header(incl.join("libtorrent").join("fwd.hpp").to_string_lossy())
-        // include dir
-        .clang_args([&format!("-I{}", incl.display()), "-xc++", "-std=c++14"])
-        // Tell cargo to invalidate the built crate whenever any of the
-        // included header files changed.
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        // Finish the builder and generate the bindings.
-        .generate()
-        // Unwrap the Result and panic on failure.
-        .expect("Unable to generate bindings");
-
-    // Write the bindings to the $OUT_DIR/bindings.rs file.
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
+    let mut b = autocxx_build::Builder::new("src/lib.rs", &[&dst.join("include")]).build()?;
+    // This assumes all your C++ bindings are in lib.rs
+    b.flag_if_supported("-std=c++14").compile("autocxx"); // arbitrary library name, pick anything
+    Ok(())
 }
--- libtorrent-sys/src/lib.rs
+++ libtorrent-sys/src/lib.rs
@@ -1,8 +1,7 @@
-//! Raw Rust bindings to the `libtorrent` C-library.
+use autocxx::prelude::*;
 
-#![allow(non_upper_case_globals, non_camel_case_types, non_snake_case)]
-
-extern crate link_cplusplus;
-extern crate openssl_sys;
-
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+include_cpp! {
+    #include "libtorrent/session.hpp"
+    safety!(unsafe)
+    generate_ns!("libtorrent")
+}
