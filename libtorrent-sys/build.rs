b98011fdc9219ec59f3b45fef49d5a334abcafe4
Jay Jackson <jj@jj.is-a.dev>
2023-02-22 23:31:25 -0500

    links on windows

--- libtorrent-sys/build.rs
+++ libtorrent-sys/build.rs
@@ -4,15 +4,18 @@ fn main() {
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=src/lib.rs");
 
+    let boost_path = std::env::var_os("BOOST_ROOT")
+        .unwrap_or_else(std::ffi::OsString::new);
+    let boost_path = std::path::Path::new(&boost_path);
+
     let mut conf = Config::new("vendor/libtorrent");
 
-    conf.define("CMAKE_CXX_STANDARD", "14")
-        .define("BUILD_SHARED_LIBS", "OFF")
-        .define("static_runtime", "ON"); // static build
+    conf.define("CMAKE_CXX_STANDARD", "17")
+        .define("BUILD_SHARED_LIBS", "OFF") // static build
+        .define("static_runtime", "ON") // static build
+        .define("BOOST_ROOT", "");
 
     let dst = conf.build();
-
-    // println!("cargo:rustc-link-lib=static=boost");
 
     println!(
         "cargo:rustc-link-search=native={}",
@@ -20,8 +23,14 @@ fn main() {
     );
     println!("cargo:rustc-link-lib=static=torrent-rasterbar");
 
-    let boost_path = std::env::var_os("BOOST_ROOT").unwrap_or_else(std::ffi::OsString::new);
-    let boost_path = std::path::Path::new(&boost_path);
+    println!("cargo:rustc-link-search=native={}", boost_path.display());
+
+    if cfg!(target_os = "windows") {
+        // link to winapi
+        println!("cargo:rustc-link-lib=dylib=Iphlpapi");
+        #[cfg(debug_assertions)]
+        println!("cargo:rustc-link-lib=dylib=DbgHelp");
+    }
 
     cxx_build::bridge("src/lib.rs")
         .include("vendor/libtorrent/include")
--- libtorrent-sys/src/lib.rs
+++ libtorrent-sys/src/lib.rs
@@ -1,15 +1,15 @@
+extern crate openssl_sys;
+
 #[cxx::bridge(namespace = "libtorrent")]
-mod ffi {
+pub mod ffi {
     unsafe extern "C++" {
         include!("libtorrent/version.hpp");
 
-        fn version() -> *const char;
+        fn version() -> *const c_char;
     }
 }
 
 #[test]
-fn test_parse_magnet_uri() {
-    unsafe {
-        ffi::parse_magnet_uri("magnet:?xt=urn:btih:2d2d2d2d2d424547494e20");
-    }
+fn test_version() {
+    panic!("version: {}", unsafe { std::ffi::CStr::from_ptr(ffi::version()).to_str().unwrap() });
 }
